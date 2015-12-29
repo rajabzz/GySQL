81e8d8df2968b7b2c434978e4d4dd2b80091f7c2
arashms <amsohi.arash@gmail.com>
2015-12-29 16:04:39 +0330

    select chaneges 2

--- db_main/src/dbms/test/Test.java
+++ db_main/src/dbms/test/Test.java
@@ -9,11 +9,6 @@ import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String... args) {
-        ArrayList<String> a = new ArrayList<>();
-
-        System.out.println(a.isEmpty());
-        for (String r: a) {
-            System.out.println(r);
-        }
+       
     }
 }
